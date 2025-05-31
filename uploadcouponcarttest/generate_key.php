3f77376fcb0086882103511a988305249a62ae5a
Shauryan Singh <probothacksidearocks@gmail.com>
2025-05-31 21:25:21 +0530

    new

--- uploadcouponcarttest/generate_key.php
+++
@@ -1,9 +0,0 @@
-<?php
-function generate_key($customer_id, $email, $secret = "mySuperSecret") {
-    $data = $customer_id . "|" . $email . "|" . $secret;
-    return base64_encode($data);
-}
-
-// Example
-echo generate_key("1", "customer@example.com");
-?>
